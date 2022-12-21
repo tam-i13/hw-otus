6024218042c89f678b424578c42d01f41ba49241
tam-i13 <46927823+tam-i13@users.noreply.github.com>
2022-12-21 20:28:22 +0000

    linter ok

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -26,22 +26,24 @@ func Unpack(s string) (string, error) {
 	}
 
 	if isDigit09(sliceRune[0]) {
-		return "", fmt.Errorf("First rune is digit. %w", ErrInvalidString)
+		return "", fmt.Errorf("first rune is digit. %w", ErrInvalidString)
 	}
 
 	for i := 0; i < len(sliceRune); i++ {
 		tmpRune := sliceRune[i]
 
-		if i+1 < len(sliceRune) && string(sliceRune[i]) == `\` && (isDigit09(sliceRune[i+1]) || string(sliceRune[i+1]) == `\`) {
-			tmpRune = sliceRune[i+1]
-			i++
-		} else if i+1 < len(sliceRune) && string(sliceRune[i]) == `\` && unicode.IsLetter(sliceRune[i+1]) {
-			return "", fmt.Errorf("After / rune. ERROR. %w", ErrInvalidString)
+		if i+1 < len(sliceRune) {
+			if string(sliceRune[i]) == `\` && (isDigit09(sliceRune[i+1]) || string(sliceRune[i+1]) == `\`) {
+				tmpRune = sliceRune[i+1]
+				i++
+			} else if i+1 < len(sliceRune) && string(sliceRune[i]) == `\` && unicode.IsLetter(sliceRune[i+1]) {
+				return "", fmt.Errorf("after / rune. ERROR. %w", ErrInvalidString)
+			}
 		}
 
 		if i+1 < len(sliceRune) && isDigit09(sliceRune[i+1]) {
 			if i+2 < len(sliceRune) && isDigit09(sliceRune[i+2]) {
-				return "", fmt.Errorf("Double digit. %w", ErrInvalidString)
+				return "", fmt.Errorf("double digit. %w", ErrInvalidString)
 			}
 			t, _ := strconv.Atoi(string(sliceRune[i+1]))
 			finalString.WriteString(strings.Repeat(string(tmpRune), t))
