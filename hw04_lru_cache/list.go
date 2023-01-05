023a1d37b328b09226c90e9b16c7305fef1d0353
tam-i13 <46927823+tam-i13@users.noreply.github.com>
2023-01-05 06:51:42 +0000

    test

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,73 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	List map[*ListItem]*ListItem
+}
+
+func (l *list) len() int {
+	return len(l.List)
+}
+
+func (l *list) Front() *ListItem {
+	tmpNode := l.List[0]
+	flag := false
+	for flag == false {
+		if tmpNode.Prev == nil {
+			return &tmpNode
+		} else {
+			tmpNode = *tmpNode.Prev
+		}
+	}
+}
+
+func (l *list) Back() *ListItem {
+	tmpNode := l.List[0]
+	for false == false {
+		if tmpNode.Next == nil {
+			return tmpNode
+		} else {
+			tmpNode = tmpNode.Next
+		}
+	}
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	front := l.Front()
+	newNode := ListItem{
+		Value: v,
+		Prev:  nil,
+		Next:  front,
+	}
+
+	l.List[&newNode] = &newNode
+	front.Prev = &newNode
+
+	return &newNode
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	back := l.Back()
+	newNode := ListItem{
+		Value: v,
+		Prev:  back,
+		Next:  nil,
+	}
+	l.List = append(l.List, &newNode)
+	back.Next = &newNode
+	return &newNode
+}
+
+func (l *list) Remove(i *ListItem) {
+
+	if i.Next != nil && i.Prev != nil {
+		i.Next.Prev = i.Prev
+		i.Prev.Next = i.Next
+	}
+	// удаление
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	return len(l.List)
 }
 
 func NewList() List {
