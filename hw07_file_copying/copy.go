60276a862b5740635bbe8ebcaffdb33c23f5cc26
tam-i13 <46927823+tam-i13@users.noreply.github.com>
2023-02-12 10:38:18 +0000

    hw07_file_copying complited

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,75 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
+	ErrFileNotExist          = errors.New("file not exist")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrOpenFile              = errors.New("open file error")
+	ErrCreateeFile           = errors.New("create file error")
+	ErrNegativLimit          = errors.New("negativ limit value")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fileStat, err := os.Stat(fromPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ErrFileNotExist
+		}
+	}
+
+	if fileStat.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if fileStat.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+
+	fileFrom, errOpen := os.Open(fromPath)
+
+	if errOpen != nil {
+		return ErrOpenFile
+	}
+	defer fileFrom.Close()
+
+	fileDist, errCreateeFile := os.Create(toPath)
+
+	if errCreateeFile != nil {
+		return ErrCreateeFile
+	}
+	defer fileDist.Close()
+
+	if limit == 0 {
+		limit = fileStat.Size()
+	}
+	if limit < 0 {
+		return ErrNegativLimit
+	}
+
+	if offset > 0 {
+		_, errFileSeek := fileFrom.Seek(offset, io.SeekStart)
+		if errFileSeek != nil {
+			return errFileSeek
+		}
+	}
+	if offset < 0 {
+		_, errFileSeek := fileFrom.Seek(offset, io.SeekEnd)
+		if errFileSeek != nil {
+			return errFileSeek
+		}
+	}
+	_, errCopy := io.CopyN(fileDist, fileFrom, limit)
+	if errCopy != nil {
+		if errors.Is(errCopy, io.EOF) {
+			return nil
+		}
+		return errCopy
+	}
+
 	return nil
 }
