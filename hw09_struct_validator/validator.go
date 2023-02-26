905eab9e6d497d241d0aba53ab2658999063fd1d
tam-i13 <46927823+tam-i13@users.noreply.github.com>
2023-02-26 20:36:50 +0000

    check

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,28 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const (
+	tag string = "validate"
+)
+
+var (
+	ErrIsNotStruct           = errors.New("is not struct")
+	ErrValueLessMin          = errors.New("value less min")
+	ErrValueMoreMax          = errors.New("value more max")
+	ErrValueNotInRange       = errors.New("value not in range")
+	ErrValueLenLess          = errors.New("len string value less")
+	ErrValueNotInRegexp      = errors.New("value not in regexp")
+	ErrValueNotInRangeString = errors.New("value not in range string")
+	ErrValidationTag         = errors.New("tag with an error")
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +31,166 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	errorString := strings.Builder{}
+	for i, vv := range v {
+		errorString.WriteString(vv.Field)
+		errorString.WriteString(" - ")
+		errorString.WriteString(vv.Err.Error())
+		if i < len(v)-1 {
+			errorString.WriteString(", ")
+		}
+	}
+	return errorString.String()
+}
+
+func newValidationError(f string, e error) ValidationError {
+	return ValidationError{
+		Field: f,
+		Err:   e,
+	}
+}
+
+func checkString(rs reflect.StructField, v string) (fValidationErrors ValidationErrors, err error) {
+	tagValue, ok := rs.Tag.Lookup(tag)
+	if !ok {
+		return fValidationErrors, nil
+	}
+
+	for _, tmp := range strings.Split(tagValue, "|") {
+		tmpRule := strings.Split(tmp, ":")
+		ruleK := tmpRule[0]
+		ruleV := tmpRule[1]
+		switch ruleK {
+		case "len":
+			ruleValue, err := strconv.Atoi(ruleV)
+			if err != nil {
+				return nil, err
+			}
+			if len(v) != ruleValue {
+				fValidationErrors = append(fValidationErrors, newValidationError(rs.Name, ErrValueLenLess))
+			}
+		case "regexp":
+			matched, err := regexp.Match(ruleV, []byte(v))
+			if err != nil {
+				return nil, err
+			}
+			if !matched {
+				fValidationErrors = append(fValidationErrors, newValidationError(rs.Name, ErrValueNotInRegexp))
+			}
+		case "in":
+			if !strings.Contains(ruleV, v) {
+				fValidationErrors = append(fValidationErrors, newValidationError(rs.Name, ErrValueNotInRangeString))
+			}
+		}
+	}
+	return fValidationErrors, nil
+}
+
+func checkInt(rs reflect.StructField, v int) (fValidationErrors ValidationErrors, err error) {
+	tagValue, ok := rs.Tag.Lookup(tag)
+	if !ok {
+		return fValidationErrors, nil
+	}
+	for _, tmp := range strings.Split(tagValue, "|") {
+		tmpRule := strings.Split(tmp, ":")
+		ruleK, ruleV := tmpRule[0], tmpRule[1]
+		switch ruleK {
+		case "min":
+			ruleValue, err := strconv.Atoi(ruleV)
+			if err != nil {
+				return nil, err
+			}
+			if v < ruleValue {
+				fValidationErrors = append(fValidationErrors, newValidationError(rs.Name, ErrValueLessMin))
+			}
+		case "max":
+			ruleValue, err := strconv.Atoi(ruleV)
+			if err != nil {
+				return nil, err
+			}
+			if v > ruleValue {
+				fValidationErrors = append(fValidationErrors, newValidationError(rs.Name, ErrValueMoreMax))
+			}
+		case "in":
+			ruleIn := strings.Split(ruleV, ",")
+			if len(ruleIn) != 2 {
+				return nil, ErrValidationTag
+			}
+			rmin, _ := strconv.Atoi(ruleIn[0])
+			rmax, _ := strconv.Atoi(ruleIn[1])
+			if rmin > v || v > rmax {
+				fValidationErrors = append(fValidationErrors, newValidationError(rs.Name, ErrValueNotInRange))
+			}
+		}
+	}
+	return fValidationErrors, nil
+}
+
+func checkSlice(rs reflect.StructField, a interface{}) (fValidationErrors ValidationErrors, err error) {
+	stringSlice, ok := a.([]string)
+	if ok {
+		for _, v := range stringSlice {
+			vErr, err := checkString(rs, v)
+			if err != nil {
+				return nil, err
+			}
+			if vErr != nil {
+				return vErr, err
+			}
+		}
+	}
+
+	intSlice, ok := a.([]int)
+	if ok {
+		for _, v := range intSlice {
+			vErr, err := checkInt(rs, v)
+			if err != nil {
+				return nil, err
+			}
+			if vErr != nil {
+				return vErr, err
+			}
+		}
+	}
+	return fValidationErrors, nil
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	var sValidationErrors ValidationErrors
+	getIn := reflect.ValueOf(v)
+
+	if getIn.Kind().String() != "struct" {
+		return ErrIsNotStruct
+	}
+
+	for i := 0; i < getIn.NumField(); i++ {
+		switch getIn.Field(i).Kind().String() {
+		case "int":
+			vErr, err := checkInt(getIn.Type().Field(i), int(getIn.Field(i).Int()))
+			if err != nil {
+				return err
+			}
+			sValidationErrors = append(sValidationErrors, vErr...)
+		case "string":
+			vErr, err := checkString(getIn.Type().Field(i), getIn.Field(i).String())
+			if err != nil {
+				return err
+			}
+			sValidationErrors = append(sValidationErrors, vErr...)
+		case "slice":
+			if getIn.Field(i).CanInterface() {
+				vErr, err := checkSlice(getIn.Type().Field(i), getIn.Field(i).Interface())
+				if err != nil {
+					return err
+				}
+				sValidationErrors = append(sValidationErrors, vErr...)
+			}
+		default:
+		}
+	}
+
+	if len(sValidationErrors) > 0 {
+		return sValidationErrors
+	}
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,13 +2,15 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
 
-// Test the function on different structures and other types.
 type (
 	User struct {
 		ID     string `json:"id" validate:"len:36"`
@@ -17,7 +19,7 @@ type (
 		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
 		Role   UserRole        `validate:"in:admin,stuff"`
 		Phones []string        `validate:"len:11"`
-		meta   json.RawMessage //nolint:unused
+		meta   json.RawMessage //nolint:nolintlint
 	}
 
 	App struct {
@@ -41,20 +43,40 @@ func TestValidate(t *testing.T) {
 		in          interface{}
 		expectedErr error
 	}{
-		{
-			// Place your code here.
-		},
-		// ...
-		// Place your code here.
+		{User{
+			ID:     "111111111111111111111111111111111111",
+			Name:   "Ivan",
+			Age:    32,
+			Email:  "test@test.test",
+			Role:   "admin",
+			Phones: []string{"79999999999"},
+			meta:   json.RawMessage{},
+		}, nil},
+		{App{Version: "12345"}, nil},
+		{Token{Header: []byte("1"), Payload: []byte("2"), Signature: []byte("3")}, nil},
+		{Response{Code: 21, Body: "body"}, ErrValidationTag},
+		{"string", ErrIsNotStruct},
+		{User{
+			ID:     "1",
+			Name:   "Ivan",
+			Age:    140,
+			Email:  "test@test.test",
+			Role:   "admin",
+			Phones: []string{"79999999999", "797999993999", "797999999999"},
+			meta:   json.RawMessage{},
+		}, errors.New("ID - len string value less, Age - value more max, Phones - len string value less")},
 	}
 
 	for i, tt := range tests {
+		tt := tt
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			tt := tt
 			t.Parallel()
-
-			// Place your code here.
-			_ = tt
+			res := Validate(tt.in)
+			if tt.expectedErr == nil {
+				require.Equal(t, tt.expectedErr, res)
+			} else {
+				require.Equal(t, tt.expectedErr.Error(), res.Error())
+			}
 		})
 	}
 }
